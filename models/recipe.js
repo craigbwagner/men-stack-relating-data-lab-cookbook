5b86e939e15da9985beb61bb719e3309690a33d9
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-07 13:56:41 -0400

    create recipe schema and model

---
+++ models/recipe.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	instructions: String,
+	owner: { type: mongoose.Schema.Types.ObjectId, require: true },
+	ingredients: [mongoose.Schema.Types.ObjectId],
+});
+
+const Ingredient = mongoose.model('Ingredient', recipeSchema);
+
+modules.export = Ingredient;
