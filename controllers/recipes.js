e3714aa80ba4f4e1e051363400f41105d7fa2e09
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-10 12:20:15 -0400

    add recipes index route

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,11 +1,11 @@
-// controllers/recipes.js
-
 const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js');
 const Recipe = require('../models/recipe.js');
 
-// router logic will go here - will be built later on in the lab
+router.get('/', async (req, res) => {
+	res.render('recipes/index.ejs');
+});
 
 module.exports = router;
