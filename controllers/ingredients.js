0e222ebd182d44985c51853a119cbaad6609f4b7
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-10 11:30:26 -0400

    add ingredients and recipes controllers

---
+++ controllers/ingredients.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const Recipe = require('../models/recipe.js');
+
+// router logic will go here - will be built later on in the lab
+
+module.exports = router;
