ee9dc2fd2dab093788853f0bcf8ef9df6148f6be
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-10 11:31:35 -0400

    add references to ingredients and profiles on recipe schema

--- models/recipe.js
+++ models/recipe.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	instructions: String,
-	owner: { type: mongoose.Schema.Types.ObjectId, require: true },
-	ingredients: [mongoose.Schema.Types.ObjectId],
+	owner: { type: mongoose.Schema.Types.ObjectId, require: true, ref: 'Profile' },
+	ingredients: { type: [mongoose.Schema.Types.ObjectId], ref: 'Ingredient' },
 });
 
 const Ingredient = mongoose.model('Ingredient', recipeSchema);
