7287c6870162bae97886f5f3ac09dd2bf4327a09
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-10 15:52:36 -0400

    correct naming of recipe model

--- models/recipe.js
+++ models/recipe.js
@@ -4,9 +4,9 @@ const recipeSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	instructions: String,
 	owner: { type: mongoose.Schema.Types.ObjectId, require: true, ref: 'Profile' },
-	ingredients: { type: [mongoose.Schema.Types.ObjectId], ref: 'Ingredient' },
+	ingredients: String,
 });
 
-const Ingredient = mongoose.model('Ingredient', recipeSchema);
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
-module.exports = Ingredient;
+module.exports = Recipe;
